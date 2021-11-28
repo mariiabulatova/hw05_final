a785c6e63584cfa865266e32f9980b00bc085eeb
mariia bulatova <mariia.bulatova@gmail.com>
2021-11-28 15:32:28 +0300

    hw05_final_try_2

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -1,22 +1,169 @@
+# import shutil
+# import tempfile
+#
+# from django.conf import settings
+# from django.core.files.uploadedfile import SimpleUploadedFile
+# from django.test import Client, TestCase, override_settings
+# from django.urls import reverse
+# from posts.models import Group, Post, User
+#
+# TEMP_MEDIA_ROOT = tempfile.mkdtemp(dir=settings.BASE_DIR)
+#
+#
+# SLUG = 'test-slug'
+# AUTHORIZED_USER_NAME = 'user'
+# AUTHORIZED_USER_AUTHOR = 'user_author'
+# TITLE = 'test-group'
+# DESCRIPTION = 'test-description'
+# POST_CREATE_PAGE = reverse('post:post_create')
+#
+# SMALL_GIF = (
+#     b'\x47\x49\x46\x38\x39\x61\x02\x00'
+#     b'\x01\x00\x80\x00\x00\x00\x00\x00'
+#     b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
+#     b'\x00\x00\x00\x2C\x00\x00\x00\x00'
+#     b'\x02\x00\x01\x00\x00\x02\x02\x0C'
+#     b'\x0A\x00\x3B'
+# )
+#
+#
+# @override_settings(MEDIA_ROOT=TEMP_MEDIA_ROOT)
+# class FormTests(TestCase):
+#     @classmethod
+#     def setUpClass(cls):
+#         super().setUpClass()
+#         # Создаем классы тестовых пользователей
+#         cls.user = User.objects.create_user(username=AUTHORIZED_USER_NAME)
+#
+#         # Создание тестовой группы:
+#         cls.group = Group.objects.create(
+#             title=TITLE,
+#             slug=SLUG,
+#             description=DESCRIPTION,
+#         )
+#
+#         # Создание тестового поста:
+#         cls.post = Post.objects.create(
+#             text='test-text',
+#             author=cls.user,
+#             group=cls.group,
+#         )
+#         cls.post_id = FormTests.post.pk
+#         cls.POST_EDIT = reverse('posts:post_edit',
+#                                 kwargs={'post_id': cls.post_id})
+#
+#     @classmethod
+#     def tearDownClass(cls):
+#         super().tearDownClass()
+#         shutil.rmtree(TEMP_MEDIA_ROOT, ignore_errors=True)
+#
+#     def setUp(self):
+#         # Создание экземпляра авторизованного пользователя (не автора поста)
+#         self.authorized_client = Client()
+#         self.authorized_client.force_login(FormTests.user)
+#         # Получение id тестового поста
+#         self.post_id = FormTests.post.id
+#
+#     def test_create_new_post_with_valid_form(self):
+#         """Новый пост создается через валидную форму."""
+#         current_posts_count = Post.objects.count()
+#
+#         form_data = {
+#             'text': 'test-text-2',
+#             'group': FormTests.group.id,
+#         }
+#         # создаем новый пост
+#         self.authorized_client.post(
+#             POST_CREATE_PAGE,
+#             data=form_data,
+#             follow=True,
+#         )
+#
+#         after_creating_new_post_count = Post.objects.count()
+#         self.assertNotEqual(after_creating_new_post_count,
+#                             current_posts_count)
+#
+#         self.assertTrue(Post.objects.filter(
+#             text='test-text-2', group=FormTests.group,).exists())
+#
+#     def test_edit_post_with_valid_form(self):
+#         """Пост редактируется через валидную форму."""
+#         current_posts_count = Post.objects.count()
+#
+#         form_data = {
+#             'text': 'test-text-3',
+#             'group': FormTests.group.id,
+#         }
+#
+#         # редактируем пост
+#         self.authorized_client.post(
+#             self.POST_EDIT,
+#             data=form_data,
+#             follow=True,
+#         )
+#
+#         after_editing_post_count = Post.objects.count()
+#         self.assertEqual(after_editing_post_count,
+#                          current_posts_count)
+#
+#         self.assertTrue(
+#             Post.objects.filter(
+#                 id=FormTests.post.id,
+#                 text='test-text-3',
+#                 group=FormTests.group,
+#             ).exists()
+#         )
+#
+#     def test_create_post_index(self):
+#         """Валидная форма создает запись в index."""
+#         current_posts_count = Post.objects.count()
+#
+#         uploaded = SimpleUploadedFile(
+#             name='small.gif',
+#             content=SMALL_GIF,
+#             content_type='image/gif'
+#         )
+#         form_data = {
+#             'text': 'test-text-4',
+#             'image': uploaded,
+#         }
+#         # Отправляем POST-запрос
+#         self.authorized_client.post(
+#             reverse('posts:post_create'),
+#             data=form_data,
+#             follow=True
+#         )
+#
+#         after_adding_post_count = Post.objects.count()
+#         # Проверяем, увеличилось ли число постов
+#         self.assertNotEqual(after_adding_post_count, current_posts_count)
+#         # Проверяем, что создалась запись с заданным текстом и картинкой
+#         self.assertTrue(
+#             Post.objects.filter(
+#                 text='test-text-4',
+#                 image='posts/small.gif'
+#             ).exists()
+#         )
+#         response = self.authorized_client.get(reverse(
+#             'posts:profile', kwargs={'username': 'user'}))
+#         self.assertContains(
+#             response,
+#             Post.objects.filter(
+#                 text='test-text-4',
+#                 image='posts/small.gif'
+#             )
+#         )
 import shutil
 import tempfile
 
-from django.conf import settings
-from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import Client, TestCase, override_settings
 from django.urls import reverse
+from django.conf import settings
+from django.core.files.uploadedfile import SimpleUploadedFile
+
 from posts.models import Group, Post, User
 
 TEMP_MEDIA_ROOT = tempfile.mkdtemp(dir=settings.BASE_DIR)
-
-
-SLUG = 'test-slug'
-AUTHORIZED_USER_NAME = 'user'
-AUTHORIZED_USER_AUTHOR = 'user_author'
-TITLE = 'test-group'
-DESCRIPTION = 'test-description'
-POST_CREATE_PAGE = reverse('post:post_create')
-
 SMALL_GIF = (
     b'\x47\x49\x46\x38\x39\x61\x02\x00'
     b'\x01\x00\x80\x00\x00\x00\x00\x00'
@@ -33,24 +180,23 @@ class FormTests(TestCase):
     def setUpClass(cls):
         super().setUpClass()
         # Создаем классы тестовых пользователей
-        cls.user = User.objects.create_user(username=AUTHORIZED_USER_NAME)
+        cls.user = User.objects.create_user(username="user")
 
         # Создание тестовой группы:
-        cls.group = Group.objects.create(
-            title=TITLE,
-            slug=SLUG,
-            description=DESCRIPTION,
+        cls.group_1 = Group.objects.create(
+            title='test-group-1',
+            slug='test-slug-1',
+            description='test-description-1',
         )
 
         # Создание тестового поста:
         cls.post = Post.objects.create(
-            text='test-text',
+            text="test-text-1",
             author=cls.user,
-            group=cls.group,
+            group=cls.group_1,
         )
-        cls.post_id = FormTests.post.pk
-        cls.POST_EDIT = reverse('posts:post_edit',
-                                kwargs={'post_id': cls.post_id})
+        # # Создаем форму, если нужна проверка атрибутов
+        # cls.form = FormTests()
 
     @classmethod
     def tearDownClass(cls):
@@ -58,6 +204,8 @@ class FormTests(TestCase):
         shutil.rmtree(TEMP_MEDIA_ROOT, ignore_errors=True)
 
     def setUp(self):
+        # Создание неавторизованного клиента
+        self.guest_client = Client()
         # Создание экземпляра авторизованного пользователя (не автора поста)
         self.authorized_client = Client()
         self.authorized_client.force_login(FormTests.user)
@@ -70,11 +218,11 @@ class FormTests(TestCase):
 
         form_data = {
             'text': 'test-text-2',
-            'group': FormTests.group.id,
+            'group': FormTests.group_1.id,
         }
         # создаем новый пост
         self.authorized_client.post(
-            POST_CREATE_PAGE,
+            reverse('posts:post_create'),
             data=form_data,
             follow=True,
         )
@@ -84,20 +232,21 @@ class FormTests(TestCase):
                             current_posts_count)
 
         self.assertTrue(Post.objects.filter(
-            text='test-text-2', group=FormTests.group,).exists())
+            text='test-text-2', group=FormTests.group_1,).exists())
 
     def test_edit_post_with_valid_form(self):
         """Пост редактируется через валидную форму."""
+        adress = reverse('posts:post_edit', args=(self.post_id,))
+
         current_posts_count = Post.objects.count()
 
         form_data = {
             'text': 'test-text-3',
-            'group': FormTests.group.id,
+            'group': FormTests.group_1.id,
         }
-
         # редактируем пост
         self.authorized_client.post(
-            self.POST_EDIT,
+            adress,
             data=form_data,
             follow=True,
         )
@@ -110,7 +259,7 @@ class FormTests(TestCase):
             Post.objects.filter(
                 id=FormTests.post.id,
                 text='test-text-3',
-                group=FormTests.group,
+                group=FormTests.group_1,
             ).exists()
         )
 
@@ -118,12 +267,21 @@ class FormTests(TestCase):
         """Валидная форма создает запись в index."""
         current_posts_count = Post.objects.count()
 
+        # small_gif = (
+        #      b'\x47\x49\x46\x38\x39\x61\x02\x00'
+        #      b'\x01\x00\x80\x00\x00\x00\x00\x00'
+        #      b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
+        #      b'\x00\x00\x00\x2C\x00\x00\x00\x00'
+        #      b'\x02\x00\x01\x00\x00\x02\x02\x0C'
+        #      b'\x0A\x00\x3B'
+        # )
         uploaded = SimpleUploadedFile(
             name='small.gif',
             content=SMALL_GIF,
             content_type='image/gif'
         )
         form_data = {
+            'title': 'test-title-4',
             'text': 'test-text-4',
             'image': uploaded,
         }
@@ -133,27 +291,15 @@ class FormTests(TestCase):
             data=form_data,
             follow=True
         )
-
         after_adding_post_count = Post.objects.count()
         # Проверяем, увеличилось ли число постов
         self.assertNotEqual(after_adding_post_count, current_posts_count)
-        # Проверяем, что создалась запись с заданным текстом и картинкой
+        # Проверяем, что создалась запись с заданным текстом
         self.assertTrue(
             Post.objects.filter(
                 text='test-text-4',
                 image='posts/small.gif'
             ).exists()
         )
-        response = self.authorized_client.get(reverse(
-            'posts:profile', kwargs={'username': 'user'}))
-        self.assertContains(
-            response,
-            Post.objects.filter(
-                image='posts/small.gif'
-            )
-        )
-    # def test_post_exist(self):
-    #     """"""
-    #     current_posts_count = Post.objects.count()
 
 # python3 manage.py test posts.tests.test_forms -v2
